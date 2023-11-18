6bf1ec79de9daa68809becc8dd3f910a4df58f4d
Vitor Rita <vhrita.dev@gmail.com>
2023-11-18 01:51:23 -0300

    feat: Initialized project

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,18 @@
+import './style.scss'
+
+function App() {
+  return (
+    <>
+      <article>
+        <div>
+          Hello World
+        </div>
+        <div>
+          World Hello
+        </div>
+      </article>
+    </>
+  )
+}
+
+export default App
