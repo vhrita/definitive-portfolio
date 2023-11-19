d8661adfaee74a6080df04e44c88324f39eaa844
Vitor Rita <vhrita.dev@gmail.com>
2023-11-18 23:11:41 -0300

    feat: Started HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,18 +1,43 @@
 import './style.scss'
 
+import Typewriter from 'typewriter-effect'
+import Avatar from '../../../public/vite.svg'
+
+const mainSkills = [
+  'React.js',
+  'Vue.js',
+  'Node.js',
+  'Adobe Experience Manager'
+]
+
 function App() {
   return (
     <>
       <article>
         <div>
-          Hello World
+          <h1>
+            Hello, I'm <b>Vitor</b>.
+          </h1>
+          <p>A FullStack Developer</p>
+          <div>
+            <span>Skilled In</span>
+            <Typewriter
+              className='skills'
+              options={{
+                strings: mainSkills,
+                autoStart: true,
+                loop: true,
+                pauseFor: 2000,
+              }}
+              />
+          </div>
         </div>
         <div>
-          World Hello
+          <img src={Avatar} alt="" />
         </div>
       </article>
     </>
-  )
+  );
 }
 
 export default App
