7e6bdaca2b10b8ad0a7691fca21bf997caafab53
Vitor Rita <vhrita.dev@gmail.com>
2023-11-19 19:38:22 -0300

    feat: Navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,30 @@
+import './style.scss'
+
+import React, { useState, useEffect } from 'react'
+
+function Navbar({items}) {
+    const [onTop, setOnTop] = useState(true);
+
+    useEffect(() => {
+        const onScroll = () => window.scrollY >= 75 ? setOnTop(false) : setOnTop(true)
+        window.removeEventListener("scroll", onScroll)
+        window.addEventListener("scroll", onScroll, { passive: true })
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+
+    const jumpTo = (id) => {
+        document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
+    }
+
+    return (
+      <nav className={onTop ? "" : "scrolled"}>
+        <div>
+          {items.map((item) => (
+            <button key={item.id} onClick={() => jumpTo(item.id)}>{item.name}</button>
+          ))}
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar
