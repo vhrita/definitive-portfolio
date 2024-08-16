766b3b58e1d2e5b9e3183a23e6944805a2ced995
Vitor Rita <vhrita.dev@gmail.com>
2024-08-16 15:53:26 -0300

    wip: development version alert before MVP ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import { useScroll, useMotionValueEvent, inView } from "framer-motion";
 
@@ -9,89 +9,99 @@ import About from "./pages/About";
 import Portfolio from "./pages/Porfolio";
 
 import LocaleContext from "./config/internationalization/LocaleContext.js";
-import i18n from './config/internationalization/i18n';
+import i18n from "./config/internationalization/i18n";
 import { useTranslation } from "react-i18next";
 
 import imageExample from "./assets/background.jpg";
 
 function App() {
-    const [locale, setLocale] = useState(i18n.language);
-    i18n.on('languageChanged', () => setLocale(i18n.language));
+	const [locale, setLocale] = useState(i18n.language);
+	i18n.on("languageChanged", () => setLocale(i18n.language));
 
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    const pages = [
-      {
-        id: "home",
-        name: t("home"),
-        social: "vertical",
-      },
-      {
-        id: "about",
-        name: t("about"),
-        social: "vertical",
-      },
-      {
-        id: "portfolio",
-        name: t("portfolio"),
-        social: "horizontal",
-      },
-    ];
+	const pages = [
+		{
+			id: "home",
+			name: t("home"),
+			social: "vertical",
+		},
+		{
+			id: "about",
+			name: t("about"),
+			social: "vertical",
+		},
+		{
+			id: "portfolio",
+			name: t("portfolio"),
+			social: "horizontal",
+		},
+	];
 
-    const projects = [
-      {
-        title: "aaaa",
-        description: "aaaa",
-        images: Array(3).fill(imageExample),
-        techs: ["HTML", "CSS3"],
-      },
-      {
-        title: "bbbb",
-        description: "aaaa",
-        images: Array(3).fill(imageExample),
-        techs: ["HTML", "CSS3"],
-      },
-      {
-        title: "cccc",
-        description: "aaaa",
-        images: Array(3).fill(imageExample),
-        techs: ["HTML", "CSS3"],
-      },
-    ];
+	const projects = [
+		{
+			title: "aaaa",
+			description: "aaaa",
+			images: Array(3).fill(imageExample),
+			techs: ["HTML", "CSS3"],
+		},
+		{
+			title: "bbbb",
+			description: "aaaa",
+			images: Array(3).fill(imageExample),
+			techs: ["HTML", "CSS3"],
+		},
+		{
+			title: "cccc",
+			description: "aaaa",
+			images: Array(3).fill(imageExample),
+			techs: ["HTML", "CSS3"],
+		},
+	];
 
-    const { scrollY } = useScroll();
-    const [position, setPosition] = useState(0);
-    const [view, setView] = useState(pages[0]);
-    const [ social, setSocial ] = useState(pages[0].social)
+	const { scrollY } = useScroll();
+	const [position, setPosition] = useState(0);
+	const [view, setView] = useState(pages[0]);
+	const [social, setSocial] = useState(pages[0].social);
 
-    useMotionValueEvent(scrollY, "change", (latest) => {
-      setPosition(latest);
-    });
+	useMotionValueEvent(scrollY, "change", latest => {
+		setPosition(latest);
+	});
 
-    useEffect(() => {
-      pages.map((item) => {
-        inView(
-          `#${item.id}`,
-          () => {
-            setView(item);
-            setTimeout(() => {
-              setSocial(item.social)
-            }, 400);
-          },
-          { amount: 0.5 }
-        );
-      });
-    }, [position]);
+	useEffect(() => {
+		pages.map(item => {
+			inView(
+				`#${item.id}`,
+				() => {
+					setView(item);
+					setTimeout(() => {
+						setSocial(item.social);
+					}, 400);
+				},
+				{ amount: 0.5 }
+			);
+		});
+	}, [position]);
 
-    return (
-      <LocaleContext.Provider value={{ locale, setLocale }}>
-        <Navbar items={pages} view={view.id} position={position} background={view.social === "vertical"} />
-        <Social orientation={social} />
-        <Home />
-        <About />
-        <Portfolio projects={projects}/>
-      </LocaleContext.Provider>
-    );
+	//TODO: Remove when MVP is ready
+	useEffect(() => {
+		alert(t("progress"));
+	}, [locale]);
+
+	return (
+		<LocaleContext.Provider value={{ locale, setLocale }}>
+			<Navbar
+				items={pages}
+				view={view.id}
+				position={position}
+				background={view.social === "vertical"}
+			/>
+			<Social orientation={social} />
+			<Home />
+			<About />
+			<Portfolio projects={projects} />
+		</LocaleContext.Provider>
+	);
 }
 
 export default App;
