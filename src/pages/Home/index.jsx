f46290ffd8c21b1606bc60fc186e53fa94ef56e7
Vitor Rita <vhrita.dev@gmail.com>
2025-07-24 16:26:28 -0300

    feat: add draw image and enhance Home component styles with animations and responsiveness

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,10 +19,23 @@ function Home() {
     <motion.div id="home">
       <motion.span
         initial={{ width: 0 }}
-        whileInView={{ width: "20%" }}
+        whileInView={{ width: "var(--first-span-width)" }}
         transition={{ duration: 1 }}
       ></motion.span>
-      <div>
+      <motion.div
+        animate={{
+          '--float-before-x': ['0px', '10px', '-8px', '12px', '0px'],
+          '--float-before-y': ['0px', '-10px', '5px', '-12px', '0px'],
+          '--float-after-x':  ['0px', '-12px', '8px', '-15px', '0px'],
+          '--float-after-y':  ['0px', '8px', '-5px', '15px', '0px'],
+        }}
+        transition={{
+          duration: 10,
+          times: [0, 0.25, 0.5, 0.75, 1],
+          repeat: Infinity,
+          ease: 'easeInOut',
+        }}
+      >
         <h1>
           {t("greatings")} <b>Vitor</b>.
         </h1>
@@ -40,7 +53,7 @@ function Home() {
           />
         </div>
         <div>
-          {[...Array(2)].map((e, i) => (
+          {[...Array(2)].map((_e, i) => (
             <motion.span
               key={i}
               whileInView={{ rotate: ["20deg", 0, "-20deg"] }}
@@ -53,7 +66,7 @@ function Home() {
             ></motion.span>
           ))}
         </div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 }
