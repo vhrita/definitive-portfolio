c926583d1178fa6afacfe528e1cf53cf1ab469d8
Vitor Rita <vhrita.dev@gmail.com>
2025-09-29 21:02:34 -0300

    Remove unused i18next-http-backend import from i18n configuration

--- src/config/internationalization/i18n.js
+++ src/config/internationalization/i18n.js
@@ -1,7 +1,5 @@
 import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
-
 import { initReactI18next } from 'react-i18next'
 
 import en from './locales/en/translation.json'
@@ -9,7 +7,6 @@ import ptBR from './locales/pt-BR/translation.json'
 import ja from './locales/ja/translation.json'
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
